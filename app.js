94ce7539c41745461292916fe02fa5a56533f8d1
gitshubhsss <shubhamranjane16@gmail.com>
2024-04-11 13:43:55 +0530

    added feactures like as soon as you open the website it will show you 3 dogs images automatically and the button text would be 'show more dogs' now when you click on the button the text will randomaly change

--- app.js
+++ app.js
@@ -2,18 +2,48 @@ let url = "https://dog.ceo/api/breeds/image/random";
 let dogImg = document.querySelector(".image");
 let btn = document.querySelector("button");
 let img=document.querySelector("img");
-
+let btnNames=["new dog","Get buetiful dog","Get more dogs","Get dogs","I love this dog","Ohh this is my dog"];
 async function getDog() {
   try {
     let res = await axios.get(url);
     let imgLink = res.data.message;
     dogImg.innerHTML=`<img src="${imgLink}">`
-    console.log(imgLink);
-    
   } catch (err) {
     console.log("Error", err);
   }
 }
-btn.addEventListener("click", () => {
+
+btn.addEventListener("click",()=>{
   getDog();
-});
+  getRandomBtn();
+})
+
+function getRandomBtn(){
+  let randInx=Math.floor(Math.random()*btnNames.length);
+  let randvalue=btnNames[randInx];
+  btn.innerText=randvalue;
+}
+
+function oneAfterOne(){
+   return new Promise((resolve,reject)=>{
+    setTimeout(() => {
+      getDog();
+      resolve();
+    }, 1500);
+   }).then((res)=>{
+    console.log(res);
+   })
+}
+
+async function showDog(){
+  for(let i=0;i<=2;i++){
+    await oneAfterOne();
+    console.log(`image ${i}`);
+  }
+  setTimeout(() => {
+    btn.innerText="Get more images";
+  }, 1000);
+}
+showDog();
+
+
